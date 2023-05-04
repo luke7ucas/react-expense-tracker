60971c151736d205a8ab26ed13b712213454229f
Lukasz L <luke7ucas@gmail.com>
2023-05-04 20:00:19 +0100

    add: Child-to-Parent component Communication

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   // THIS IS AN EXAMPLE OF JSX WORKING UNDER THE HOOD FOR THE SYNTAX BELOW
   // return React.createElement(
   //   "div",
@@ -36,7 +41,7 @@ const App = () => {
   // THIS IS AN EXAMPLE OF JSX SYNTAX
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+  // props comes from "onSaveExpenseData" in NewExpense.js -> line: <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -59,7 +61,8 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    // console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
