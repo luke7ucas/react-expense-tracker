26498ee851a265259635caada71c61272c9f26f8
Lukasz L <luke7ucas@gmail.com>
2023-05-05 10:11:22 +0100

    add: dynamic expense array generation

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,26 +19,20 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+
+        {/* THE FOLLOWING IS NO LONGER REQUIRED, AND IS REPLACED BY THE ABOVE CODE 
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        ></ExpenseItem>
+        ></ExpenseItem> */}
       </Card>
     </div>
   );
